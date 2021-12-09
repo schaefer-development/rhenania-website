2bb581f7a60356f8de0eea8807af8f7d968d57e1
Schaeferjo <mail@joschaefer.com>
2021-12-09 18:26:00 +0100

    feat: add custom rhenania corporate colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,7 @@
 	export let data = { page: {} };
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-rc_red">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <Page page={data.page} />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,63 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+
 	purge: ['./src/**/*.html', './src/**/*.svelte'],
+	plugins: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Red Hat Text', 'Helvetica', 'Arial', 'sans-serif']
+			  }
+		},
+		colors: {
+			...colors,
+			rc_darkblue: {
+				darkest:'#15254f',
+				darker: '#203776',
+				DEFAULT: '#2a499d',
+				lighter: '#556db1',
+				lightest:'#7f92c4'
+			},
+
+			rc_skyblue: {
+				darkest:'#526a80',
+				darker: '#7a9fbf',
+				DEFAULT: '#a3d4ff',
+				lighter: '#b5ddff',
+				lightest:'#c8e5ff'
+			  },
+			  rc_red: {
+				darker: '#b02629',
+				DEFAULT: '#ea3336',
+				lighter: '#ee5c5e'
+			  },
+			  rc_green: {
+				darker: '#43801c',
+				DEFAULT: '#59aa25',
+				lighter: '#7abb51'
+			  },
+			  rc_turquoise: {
+				darker: '#1c807d',
+				DEFAULT: '#25aaa7',
+				lighter: '#7cccca'
+			  },
+			  rc_petrol: {
+				darker: '#186691',
+				DEFAULT: '#2088c1',
+				lighter: '#79b8da'
+			  },
+			  rc_purple: {
+				darker: '#4a1457',
+				DEFAULT: '#631b75',
+				lighter: '#824990'
+			  }
+		  },
+
 	},
 	variants: {
 		extend: {}
 	},
-	plugins: []
+
 };
