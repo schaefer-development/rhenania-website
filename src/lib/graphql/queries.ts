bb32f580471ff4c5241e586b5124b612b11838dc
Schaeferjo <mail@joschaefer.com>
2021-12-10 15:36:11 +0100

    feat: update queries via graphcms

--- src/lib/graphql/queries.ts
+++ src/lib/graphql/queries.ts
@@ -1,41 +1,60 @@
 import { gql } from '$lib/helpers';
 export const START = gql`
-	{
-		page(where: { slug: "start" }) {
-			id
-			slug
-			modules {
-				__typename
-				... on Eyecatcher {
-					id
-					headline
-					subheadline
-					buttonLabel
-				}
-				... on Divider {
-					id
-					padding
-				}
-				... on CardsContainer {
-					id
-					headline
-					cards {
-						__typename
-						... on CardImage {
-							id
-							image {
-								url
-							}
-						}
-						... on CardIcon {
-							id
-							icon {
-								url
-							}
-						}
-					}
-				}
-			}
+{
+	page(where:{slug:"start"}) {
+	  id
+	  modules{
+			 __typename
+		... on Divider{
+		  padding
 		}
+		... on Eyecatcher{
+		  headline
+		  subheadline
+		  image {
+			id
+			url
+		  }
+		}
+		... on Faq{
+		  question
+		  answer{
+			markdown
+		  }
+		}
+		... on HeroImage{
+		  image{
+			id
+			url
+		  }
+		  icon{
+			id
+			url
+		  }
+		}
+		...on ArticleHeroTeaser{
+		  headline
+		  subheadline
+		  teaserText
+		  heroBackgroundImage{
+			id
+			url
+		  }
+		}
+			  ... on CardsContainer{
+		  headline
+		  cards{
+			__typename
+			... on CardImage{
+			  image{
+				id
+				url
+			  }
+			  headline
+			}
+		  }
+		}
+	  }
 	}
+  }
 `;
