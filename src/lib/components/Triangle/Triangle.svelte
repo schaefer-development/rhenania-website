087ef97151fb2e966e019daeb4f20c0fd89e1b02
Schaeferjo <mail@joschaefer.com>
2021-12-10 15:36:54 +0100

    feat: custom styling

---
+++ src/lib/components/Triangle/Triangle.svelte
@@ -0,0 +1,6 @@
+<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 350 420" style="enable-background:new 0 0 350 420;" xml:space="preserve" fill="currentColor">
+<g class="st0">
+	<polygon class="st1" points="0,419.5 350,-0.5 0,-0.5 	"/>
+</g>
+</svg>
