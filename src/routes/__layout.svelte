226a45a95c7c2db0e9002ae57148c6d9242aa14d
Schaeferjo <mail@joschaefer.com>
2021-12-10 11:10:42 +0100

    feat: sticky footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,12 +9,17 @@
 <svelte:head>
 	<title>Rhenania Computer</title>
 	<meta name="description" content="Software zum Optimieren des Versandprozesses aus Aegidenberg (Bad Honnef)" />
+	
 </svelte:head>
 
+
+
+<div class="flex flex-col h-screen justify-between">
 <Header />
 
-<main>
+<main class="mb-auto">
 	<slot />
 </main>
 
 <Footer />
+</div>
