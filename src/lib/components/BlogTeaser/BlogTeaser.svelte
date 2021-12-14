e2af7b71886dbb0d09b287afcf1594035a60b51b
Schaeferjo <mail@joschaefer.com>
2021-12-14 22:14:11 +0100

    feat: blogteaser module

--- src/lib/components/BlogTeaser/BlogTeaser.svelte
+++ src/lib/components/BlogTeaser/BlogTeaser.svelte
@@ -1,3 +1,31 @@
-<div class="blogteaser">
-	<h1>BlogTeaser</h1>
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<div class="heroStart w-full overflow-hidden h-50v">
+	<div
+		class="bg_layer absolute w-full h-50v bg-cover bg-center "
+		style="background-image: url({base}/hero_example2.jpg)"
+	/>
+
+	<div
+		class="content relative z-2 w-full h-full max-w-screen-2xl mx-auto px-10 flex flex-col justify-center"
+	>
+		<div class="blogteaserbg w-full max-w-2xl inline-block px-10 py-10">
+			<h2 class="rc_h2 text-rc_skyblue">Warenversandlösung F95 erweitert</h2>
+			<h3 class="font-medium text-white">
+				Rhenania integriert Ländernachweis in Versandsoftware F95
+			</h3>
+			<hr class="border-b-1 border-rc_darkblue-darkest my-4" />
+			<p class="text-white w-9/12">
+				In Zusammenarbeit mit der Deutschen Post AG und TOSHIBA TEC Europe haben wir eine Lösung
+				entwickelt und den Ländernachweis in die Versandlösung F95 Paket- und Palettenversand
+				integriert.
+			</p>
+			<button
+				class="relative mt-8 pl-5 pr-3 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker rc_button_corner"
+				>mehr erfahren</button
+			>
+		</div>
+	</div>
 </div>
