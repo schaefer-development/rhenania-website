981719ded663508d573b6aea7a86e955d334b7a2
Schaeferjo <mail@joschaefer.com>
2021-12-14 16:36:45 +0100

    feat: add leaflet card with mapmarker

---
+++ src/lib/components/Map/Map.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from 'svelte';
+  import { base } from '$app/paths';
+  export let iconOptions = {
+    iconUrl: `${base}/mapmarker.png`,
+    iconSize: [40, 50],
+    iconAnchor: [20, 50]
+  };
+  export let layerOptions = [
+    'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png ',
+    {
+      attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+      maxZoom: 19
+    }
+  ];
+  export let zoom = 13;
+  export let coordinates = [50.6630751, 7.3039274];
+
+  export let mapAttributes = {
+    style: 'width: 100%; height: 300px;'
+  };
+
+  let map;
+  onMount(async () => {
+    await import('leaflet');
+    const icon = L.icon(iconOptions);
+    const map = L.map('map').setView(coordinates, zoom);
+    L.tileLayer(...layerOptions).addTo(map);
+    L.marker(coordinates, { icon }).addTo(map);
+
+    return () => map && map.remove();
+  });
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+</svelte:head>
+
+<div id="map" {...mapAttributes} />
--- src/lib/components/Page/Page.svelte
+++ src/lib/components/Page/Page.svelte
@@ -4,14 +4,29 @@
 	import CardsContainer from '$lib/components/CardsContainer/CardsContainer.svelte';
 	import Eyecatcher from '$lib/components/Eyecatcher/Eyecatcher.svelte';
 	import Faq from '$lib/components/Faq/Faq.svelte';
+	import CardImage from '$lib/components/CardImage/CardImage.svelte';
 	import type { Page } from '$lib/graphql/generated/schema';
 	export let page: Partial<Page> = {};
+
+
+	import Map from '$lib/components/Map/Map.svelte';
+	const mapAttributes = {
+	  class: '',
+	  style: 'width: 100%; height: 100%; min-height:500px; max-height:40vh;'
+	};
+  
+  
+	export const prerender = true;
 </script>
 
+
+
+<Map {mapAttributes} />
+
 <HeroStart />
+<CardImage />
+
 <Faq />
-
-
 
 <h1>Ich bin die Page componente für /{page.slug}</h1>
 <div class="relative">
