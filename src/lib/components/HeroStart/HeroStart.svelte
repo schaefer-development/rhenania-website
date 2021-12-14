511a705bf5d01c045336ed51cb954b869cf3b890
Schaeferjo <mail@joschaefer.com>
2021-12-14 12:33:52 +0100

    feat: herostart styling

--- src/lib/components/HeroStart/HeroStart.svelte
+++ src/lib/components/HeroStart/HeroStart.svelte
@@ -9,7 +9,7 @@
 	<div class="content relative z-2 w-full h-full max-w-screen-2xl mx-auto px-16 flex flex-col items-center justify-center text-center">
 		<h1 class="font-bold text-4xl md:text-5xl lg:text-6xl uppercase tracking-wider text-white pb-2 rc_text_shadow">Wir optimieren Ihren Versand</h1>
 		<h2 class="rc_h2 text-rc_skyblue rc_text_shadow">Software und Systeme für Versandlogistik</h2>
-		<button class="relative mt-8 pl-6 pr-2 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest rc_button_corner">mehr erfahren</button>
+		<button class="relative mt-8 pl-5 pr-3 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker rc_button_corner">mehr erfahren</button>
 	</div>
 </div>
 
