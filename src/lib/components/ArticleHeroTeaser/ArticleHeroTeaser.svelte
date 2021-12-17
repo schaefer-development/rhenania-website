0a1c2c13e2b851c1b6679160bba0ed54f0484961
Schaeferjo <mail@joschaefer.com>
2021-12-17 16:43:17 +0100

    feat: articleheroteaser module

--- src/lib/components/ArticleHeroTeaser/ArticleHeroTeaser.svelte
+++ src/lib/components/ArticleHeroTeaser/ArticleHeroTeaser.svelte
@@ -1,3 +1,26 @@
-<div>
-	<h1>ArticleHeroTeaser</h1>
+<script lang="ts">
+	import { base } from '$app/paths';
+	import type { ArticleHeroTeaser } from '$lib/graphql/generated/schema';
+	export let module: Partial<ArticleHeroTeaser> = {};
+</script>
+
+<div class=" w-full overflow-hidden h-50v">
+	<div
+		class="bg_layer absolute w-full h-50v bg-cover bg-center "
+		style="background-image: url({module.heroBackgroundImage.url})"
+	/>
+	<div class="relative z-2 h-full flex items-center max-w-screen-md mx-auto px-10">
+		<div class="blogteaserbg px-10 py-10">
+			<h2 class="rc_h1 text-rc_skyblue">{module.headline}</h2>
+			<h3 class="text-white text-lg font-medium">{module.subheadline}</h3>
+			<hr class="border-rc_darkblue-darkest my-4" />
+			<p class="text-white w-10/12">
+				{module.teaserText}
+			</p>
+			<button
+				class="relative mt-8 pl-5 pr-3 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker rc_button_corner"
+				>mehr erfahren</button
+			>
+		</div>
+	</div>
 </div>
--- src/lib/components/Page/Page.svelte
+++ src/lib/components/Page/Page.svelte
@@ -3,18 +3,19 @@
 	import CardsContainer from '$lib/components/CardsContainer/CardsContainer.svelte';
 	import Eyecatcher from '$lib/components/Eyecatcher/Eyecatcher.svelte';
 	import FaqContainer from '$lib/components/FaqContainer/FaqContainer.svelte';
+	import ArticleHeroTeaser from '$lib/components/ArticleHeroTeaser/ArticleHeroTeaser.svelte';
 	import type { Page } from '$lib/graphql/generated/schema';
 	export let page: Partial<Page>;
 	const mapping = {
 		Divider,
 		CardsContainer,
 		Eyecatcher,
-		FaqContainer
+		FaqContainer,
+		ArticleHeroTeaser
 	};
 </script>
 
 {#if page}
-	<h1>Ich bin die Page componente für /{page.slug}</h1>
 	<div class="relative">
 		{#each page.modules as module (module.id)}
 			<svelte:component this={mapping[module.__typename]} {module} />
