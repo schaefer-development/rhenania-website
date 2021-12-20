c584d9d04f0be7e64a4afbe510297d568dae437a
Schaeferjo <mail@joschaefer.com>
2021-12-20 18:16:50 +0100

    feat: divider styling with container and background-color

--- src/lib/components/Divider/Divider.svelte
+++ src/lib/components/Divider/Divider.svelte
@@ -4,15 +4,16 @@
 </script>
 
 <div class="divider w-full pt-10 pt-{module.paddingTop} pb-10 pb-{module.paddingBottom}">
-	<div class="h-1/2 w-full border-gray-300 border-b-4" />
-	<div class="mx-auto max-w-screen-2xl w-full border border-rc_red flex flew-row">
-		<div class="bg-gray-300 w-3/12" />
-		<div class="w-1/12" />
-		<div class="bg-gray-300 w-1/12" />
-		<div class="w-1/12" />
-		<div class="bg-gray-300 w-1/12" />
-		<div class="w-1/12" />
-		<div class="bg-gray-300 w-1/12" />
-		<div class="w-1/12" />
+	<div class="h-1 w-full border-gray-300 border-b-4" />
+	<div class="mx-auto max-w-screen-2xl w-full h-1 -mt-1 flex flew-row">
+		<div class="bg-gray-300" style="width:50%;" />
+		<div class="bg-gray-50" style="width:6%;" />
+		<div class="bg-gray-300" style="width:5%;" />
+		<div class="bg-gray-50" style="width:6%;" />
+		<div class="bg-gray-300" style="width:7%;" />
+		<div class="bg-gray-50" style="width:8%;" />
+		<div class="bg-gray-300" style="width:8%;" />
+		<div class="bg-gray-50" style="width:4%;" />
+		<div class="bg-gray-300" style="width:6%;" />
 	</div>
 </div>
