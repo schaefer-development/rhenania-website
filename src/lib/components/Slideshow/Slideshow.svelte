501931af3c679abbfca10ac8625004c84785fcac
Schaeferjo <mail@joschaefer.com>
2022-01-20 17:02:54 +0100

    feat: working on slideshow

--- src/lib/components/Slideshow/Slideshow.svelte
+++ src/lib/components/Slideshow/Slideshow.svelte
@@ -2,16 +2,22 @@
 	import { base } from '$app/paths';
 	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 	import '@splidejs/splide/dist/css/splide.min.css';
+
+
+	
 </script>
 
 <div>
 	<Splide
 		options={{
+			autoplay:true,
 			rewind: true,
 			perPage: 5,
 			perMove: 1,
 			gap: '1rem',
-
+			autoScroll: {
+    speed: 2,
+  },
 			classes: {
 				arrows: 'splide__arrows your-class-arrows',
 				arrow: 'splide__arrow your-class-arrow',
@@ -20,34 +26,31 @@
 			}
 		}}
 	>
-		<SplideSlide>
-			<img src="{base}/logo_clients/clients_jumo.svg" alt="Jumo" class="w-10/12 mx-auto" />
+    <SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_jumo.png" alt="Jumo" class="w-10/12 mx-auto" />
 		</SplideSlide>
-		<SplideSlide>
-			<img src="{base}/logo_clients/clients_lamy.svg" alt="Lamy" class="w-10/12 mx-auto" />
-		</SplideSlide>
-
-		<SplideSlide>
-			<img src="{base}/logo_clients/clients_duravit.svg" alt="Duravit" class="w-10/12 mx-auto" />
+	    <SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_lamy.png" alt="Lamy" class="w-10/12 mx-auto" />
 		</SplideSlide>
 
-		<SplideSlide>
-			<img src="{base}/logo_clients/clients_abus.svg" alt="Abus" class="w-10/12 mx-auto" />
+		<SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_duravit.png" alt="Duravit" class="w-10/12 mx-auto" />
 		</SplideSlide>
-		<SplideSlide>
-			<img src="{base}/logo_clients/clients_fujifilm.svg" alt="Fujifilm" class="w-10/12 mx-auto" />
+
+	    <SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_abus.png" alt="Abus" class="w-10/12 mx-auto" />
 		</SplideSlide>
-		<SplideSlide>
-			<img
-				src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg"
-				alt="Bild 2"
-			/>
+		<SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_fujifilm.png" alt="Fujifilm" class="w-10/12 mx-auto" />
 		</SplideSlide>
-		<SplideSlide>
-			<img
-				src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg"
-				alt="Bild 2"
-			/>
+		<SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_vanlaack.png" alt="Fujifilm" class="w-10/12 mx-auto" />
+		</SplideSlide>
+		<SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_igepa.png" alt="Fujifilm" class="w-10/12 mx-auto" />
+		</SplideSlide>	
+		<SplideSlide data-splide-interval="1000">
+			<img src="{base}/logo_clients/clients_dom.png" alt="Fujifilm" class="w-10/12 mx-auto" />
 		</SplideSlide>
 	</Splide>
 </div>
