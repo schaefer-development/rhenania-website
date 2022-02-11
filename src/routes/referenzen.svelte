0b8845f75a65a846c06d6b53ae3d74af4c7455d5
Schaeferjo <mail@joschaefer.com>
2022-02-11 21:26:01 +0100

    feat: add page content => clients logos

--- src/routes/referenzen.svelte
+++ src/routes/referenzen.svelte
@@ -1,13 +1,178 @@
-<script context="module" lang="ts">
-	import Page from '$lib/components/Page/Page.svelte';
-	import type { LoadPageData } from '$lib/helpers';
-	import { LoadPage } from '$lib/helpers';
-	export const load = LoadPage('referenzen');
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	export let data: LoadPageData = { page: null };
+	import { base } from '$app/paths';
 </script>
 
-<Page page={data.page} />
+<div
+	class="relative z-10 max-w-screen-lg mx-auto px-10 pt-12 pb-12 md:pb-24 grid grid-cols-1 lg:grid-cols-8 gap-10"
+>
+	<div class="col-span-full">
+		<h1 class="rc_h1 text-rc_darkblue">Referenzen</h1>
+		<h3 class="rc_h3 py-2">Hier finden Sie einen Auszug unserer Referenzkunden.</h3>
+	</div>
+
+	<div class="col-span-full md:col-span-5 font-bold">
+		Gerne nennen wir Ihnen für Ihren konkreten Anwendungsfall weitere Referenzen – auch mit
+		Ansprechpartner und Telefonnummer.
+	</div>
+
+	<div class="col-span-full md:col-span-3 lg:text-right">
+		<a
+			href="{base}/kontakt"
+			class="relative pl-5 pr-3 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker rc_button_corner"
+			>Jetzt Kontakt aufnehmen</a
+		>
+	</div>
+</div>
+
+<div
+	class="relative z-10 max-w-screen-lg mx-auto px-10 pb-24 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-2"
+>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_abus.svg" alt="Abus" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_aokverlag.svg" alt="AOK Verlag" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_aperam.svg" alt="Aperam" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_bernstein.svg" alt="Bernstein" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_damstahl.svg" alt="Damstahl" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_detax.svg" alt="Detax" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_dom.svg" alt="DOM" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_dometic.svg" alt="Dometic" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img
+			src="{base}/logo_clients/clients_drittewelt.svg"
+			alt="Dritte Welt Shop"
+			class="w-full mx-auto"
+		/>
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_duravit.svg" alt="Duravit" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img
+			src="{base}/logo_clients/clients_duttenhofer.svg"
+			alt="Duttenhofer"
+			class="w-full mx-auto"
+		/>
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_ekz.svg" alt="EKZ" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_eska.svg" alt="ESKA" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_esv.svg" alt="ESV" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img
+			src="{base}/logo_clients/clients_fahrenberger.svg"
+			alt="Fahrenberger"
+			class="w-full mx-auto"
+		/>
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_flintec.svg" alt="Flintec" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_fujifilm.svg" alt="Fujifilm" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_gewa.svg" alt="Gewa" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_gondrom.svg" alt="Gondrom" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_hagesued.svg" alt="HageSüd" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_hoermann.svg" alt="Hörmann" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_igepa.svg" alt="IGEPA" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_intenso.svg" alt="Intenso" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_jordan.svg" alt="Jordan" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_jumo.svg" alt="Jumo" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_kindundco.svg" alt="Kind&Co" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_labothene.svg" alt="Labothene" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_lamy.svg" alt="Lamy" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_lr.svg" alt="LR" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_marbach.svg" alt="Marbach" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_meindl.svg" alt="Meindl" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_noegel.svg" alt="Nögel" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_oui.svg" alt="Oui" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_perschmann.svg" alt="Perschmann" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_picard.svg" alt="Picard" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_rrteam.svg" alt="RR Team" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img
+			src="{base}/logo_clients/clients_schlengermann.svg"
+			alt="Schlengermann"
+			class="w-full mx-auto"
+		/>
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_techem.svg" alt="Techem" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_troeber.svg" alt="Tröber" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_tucker.svg" alt="Tucker" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_vanlaack.svg" alt="van Laack" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_volz.svg" alt="Volz" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_westaflex.svg" alt="Westaflex" class="w-full mx-auto" />
+	</div>
+	<div class="col-span-1 border bg-white">
+		<img src="{base}/logo_clients/clients_wundw.svg" alt="W und W" class="w-full mx-auto" />
+	</div>
+</div>
