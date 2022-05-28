264027b34a2936bbbba739701f67ff34f4e7ec5b
Robert Schäfer <git@roschaefer.de>
2022-05-28 20:36:01 -0300

    chore: remove internationalization
    As desired, this removes the internationalization and uses `de` as
    hard-coded fallback language, ignoring `Accept-Language` header.

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,9 @@ import { localeCodes, fallbackLocale } from '$lib/i18n';
 import acceptLanguage from 'accept-language';
 acceptLanguage.languages(localeCodes);
 
-export const getSession: GetSession = (event) => {
-	const lang = acceptLanguage.get(event.request.headers.get('accept-language')) ?? fallbackLocale;
+export const getSession: GetSession = (_event) => {
+	// TODO: Why does this work locally but for whatever reason not on Netlify?
+	// const lang = acceptLanguage.get(event.request.headers.get('accept-language')) ?? fallbackLocale;
+	const lang = fallbackLocale;
 	return { lang };
 };
