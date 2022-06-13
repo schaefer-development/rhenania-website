a81018d1a68a26f1522d63b5ee3bc32b1763c4d8
Robert Schäfer <git@roschaefer.de>
2022-06-13 13:08:00 -0300

    feat: style <em> highlights

--- src/lib/components/SearchResult/SearchResult.svelte
+++ src/lib/components/SearchResult/SearchResult.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <h2>{@html hit.__typename}</h2>
+<p>{@html hit.title}</p>
+<p>{@html hit.description}</p>
+<p>{@html hit.teaserText}</p>
+
+<style>
+	/* highlights text matching the search string */
+	:global(em) {
+		color: darkcyan;
+		line-height: 1.2em;
+		border-radius: 3pt;
+		font-style: normal;
+	}
+</style>
