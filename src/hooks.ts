8c056f8a86bc5b29552c50ab5a49ed8649d8a644
Robert Schäfer <git@roschaefer.de>
2022-06-14 14:14:42 -0300

    hotfix: never upload search index

--- src/hooks.ts
+++ src/hooks.ts
@@ -20,7 +20,7 @@ const appId = ALGOLIA_APP_ID;
 const apiKey = ALGOLIA_ADMIN_KEY;
 
 // only update Algolia indices if required env vars are defined
-if (dev === false && appId && apiKey) {
+if (false && appId && apiKey) {
 	// update Algolia search indices on production builds
 	const algoliaConfig = {
 		appId,
