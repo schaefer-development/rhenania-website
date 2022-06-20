2cba3e07861c0ee928e4977d9b5b9682c35a5316
Jo Schäfer <mail@joschaefer.com>
2022-06-20 12:53:05 +0200

    feat: cards container headline not requiered any more

--- src/lib/components/CardsContainer/CardsContainer.svelte
+++ src/lib/components/CardsContainer/CardsContainer.svelte
@@ -13,7 +13,11 @@
 <div id={module.headline} class="mod_cardsContainer relative">
 	<div class="relative {mapPadding(module.paddingTop)}" />
 	<div class="max-w-screen-2xl px-4 lg:px-10 mx-auto py-6 lg:py-12">
-		<h1 id={module.headline} class="rc_h1 text-rc_darkblue text-center pb-4">{module.headline}</h1>
+		{#if module?.headline}
+			<h1 id={module.headline} class="rc_h1 text-rc_darkblue text-center pb-4">
+				{module.headline}
+			</h1>
+		{/if}
 		<div class="flex flex-row flex-wrap justify-left xl:justify-center">
 			{#each module.cards as card (card.id)}
 				<div class="flex items-stretch w-full md:w-6/12 2xl:w-3/12">
