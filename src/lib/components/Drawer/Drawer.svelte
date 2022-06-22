531e30d0bdad025dd5300c157956408c05d0dcba
Robert Schäfer <git@roschaefer.de>
2022-06-22 14:22:22 -0300

    feat: close menu button

--- src/lib/components/Drawer/Drawer.svelte
+++ src/lib/components/Drawer/Drawer.svelte
@@ -7,8 +7,12 @@
 <aside
 	on:click={() => toggleMenu(menuLabel)}
 	class="{openMenu === menuLabel
-		? 'rc_menu_visible hidden lg:block'
-		: 'rc_menu_hidden'} rc_menu left-0 top-20 w-full shadow-md fixed overflow-auto ease-in-out transform transition-all duration-300 z-40"
+		? 'rc_menu_visible'
+		: 'rc_menu_hidden'} flex flex-col items-end rc_menu left-0 top-20 w-full shadow-md fixed overflow-auto ease-in-out transform transition-all duration-300 z-40"
 >
 	<slot />
+	<button
+		class="px-6 py-3 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker"
+		>X</button
+	>
 </aside>
