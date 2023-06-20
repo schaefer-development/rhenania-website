0b92c6afc6041ee42ab3f32850f2181e7864dec9
joschaefer <mail@joschaefer.com>
2023-06-20 15:50:26 +0200

    feat: add contactformat as accordion

--- src/routes/[lang=localeCode]/kontakt/index.svelte
+++ src/routes/[lang=localeCode]/kontakt/index.svelte
@@ -29,8 +29,8 @@
 	};
 </script>
 
-<div class="contact_form col-span-2">
-	<h2 class="rc_h1 text-rc_darkblue headlinelink">Kontaktieren Sie uns direkt per E-Mail an</h2>
+<div class="col-span-2">
+	<h2 class="rc_h1 text-rc_darkblue headlinelink pb-3">Kontaktieren Sie uns per E-Mail</h2>
 
 	<div class="relative flex justify-center">
 		<a
@@ -55,8 +55,153 @@
 		</div>
 	</div>
 
-	<h3 class="font-bold text-black pt-6 px-1 uppercase tracking-wider">Kontaktformular</h3>
+	<div class="mod_contactFormAccordion relative">
+		<div class="relative w-full" />
+		<div class="mod_accordion relative tab w-full overflow-hidden h-auto pb-4 block">
+			<input class="absolute opacity-0 " id="contactform" type="checkbox" name="tabs" />
+			<label class="cursor-pointer flex border-b-2 py-2" for="contactform">
+				<h3 class="w-full font-bold">Oder Sie nutzen das Kontaktformular</h3>
+				<div class="icon">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-8 w-8"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+						/>
+					</svg>
+				</div>
+			</label>
+			<div class="tab-content h-auto">
+				<div class="pt-4 pb-8">
+					<form class="w-full pt-4" action={$linkTo(`/kontakt.json`)} method="post">
+						<div class="flex flex-wrap -mx-3 mb-6">
+							<div class="w-full px-3 mb-10">
+								<label
+									class="block uppercase tracking-widest text-gray-700 text-xs px-1 font-bold "
+								>
+									<input
+										class="appearance-none block w-full bg-white text-gray-700 text-lg border border-gray-400 py-3 px-3 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-black"
+										name="firma"
+										type="text"
+										placeholder="Musterfirma"
+									/>
+									Firma (Optional)
+								</label>
+							</div>
 
+							<div class="w-full px-3 mb-10">
+								<label class="block uppercase tracking-widest text-gray-700 text-xs px-1 font-bold">
+									<input
+										class="appearance-none block w-full bg-white text-gray-700 text-lg border border-gray-400 py-3 px-3 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-black"
+										name="name"
+										type="text"
+										placeholder="Max Mustermann"
+									/>
+									Name
+								</label>
+							</div>
+
+							<div class="w-full px-3 mb-10">
+								<label
+									class="block uppercase tracking-widest text-gray-700 text-xs px-1 font-bold "
+								>
+									<input
+										class="appearance-none block w-full bg-white text-gray-700 text-lg border border-gray-400 py-3 px-3 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-black"
+										name="emailAdresse"
+										type="email"
+										placeholder="mail@beispiel.de"
+									/>
+									E-Mail
+								</label>
+							</div>
+
+							<div class="w-full px-3 mb-4">
+								<label
+									class="block uppercase tracking-widest text-gray-700 text-xs px-1 font-bold "
+								>
+									<input
+										class="appearance-none block w-full bg-white text-gray-700 text-lg border border-gray-400 py-3 px-3 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-black"
+										name="telefon"
+										type="text"
+										placeholder="0123456789"
+									/>
+									Telefon (Optional)
+								</label>
+							</div>
+
+							<div class="w-full px-3">
+								<label
+									class="block uppercase tracking-widest text-gray-700 text-xs px-1 font-bold mb-1"
+								>
+									<textarea
+										class=" no-resize appearance-none block w-full bg-white text-gray-700 text-lg border border-gray-400 py-3 px-3 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-black h-48 resize-none"
+										name="nachricht"
+									/>
+									Ihre Nachricht
+								</label>
+							</div>
+
+							<div class="py-6 flex justify-center relative border w-full">
+								<HCaptcha
+									sitekey={HCAPTCHA_SITEKEY}
+									hl="de"
+									bind:this={captcha}
+									on:error={handleError}
+									on:success={handleSuccess}
+								/>
+								<noscript>
+									Sie benötigen einen JavaScript-fähigen Browser für die Kontaktanfrage per Browser.
+									Alternativ können Sie uns auch gerne eine E-Mail senden.
+								</noscript>
+							</div>
+
+							<div class="mb-6 flex flex-row items-center w-full">
+								<div>
+									<input type="checkbox" bind:checked={datapolicy} />
+								</div>
+								<div class="px-3 dataprivacy">
+									Ich habe die <a
+										href={$linkTo('/datenschutz/')}
+										class=" font-bold text-rc_red hover:text-rc_red focus:ring-0 focus:outline-none focus:text-rc_red"
+										>Datenschutzerklärung</a
+									>
+									gelesen und stimme dieser zu.
+								</div>
+							</div>
+
+							<div class="md:flex md:items-center">
+								<div class="md:w-1/3">
+									{#if browser}
+										{#if valid && datapolicy}
+											<button
+												class="relative mt-4 pl-5 pr-3 py-2 bg-rc_red text-white text-sm font-bold uppercase tracking-widest hover:bg-rc_red-darker rc_button_corner"
+												type="submit">Nachricht absenden</button
+											>
+										{:else}
+											<button
+												disabled
+												class="relative mt-4 pl-5 pr-3 py-2 bg-gray-300 text-white text-sm font-bold uppercase tracking-widest rc_button_corner"
+												type="submit">Nachricht absenden</button
+											>
+										{/if}
+									{/if}
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="w-full h-24 border" />
 	<!-- contact form-->
 	<form class="w-full pt-4" action={$linkTo(`/kontakt.json`)} method="post">
 		<div class="flex flex-wrap -mx-3 mb-6">
@@ -120,19 +265,19 @@
 				</label>
 			</div>
 		</div>
-		<div class="pb-8 flex justify-center">
-			<HCaptcha
-				sitekey={HCAPTCHA_SITEKEY}
-				hl="de"
-				bind:this={captcha}
-				on:error={handleError}
-				on:success={handleSuccess}
-			/>
-			<noscript>
-				Sie benötigen einen JavaScript-fähigen Browser für die Kontaktanfrage per Browser.
-				Alternativ können Sie uns auch gerne eine E-Mail senden.
-			</noscript>
-		</div>
+		<!--	<div class="pb-8 flex justify-center relative z-1">
+				<HCaptcha
+					sitekey={HCAPTCHA_SITEKEY}
+					hl="de"
+					bind:this={captcha}
+					on:error={handleError}
+					on:success={handleSuccess}
+				/>
+				<noscript>
+					Sie benötigen einen JavaScript-fähigen Browser für die Kontaktanfrage per Browser.
+					Alternativ können Sie uns auch gerne eine E-Mail senden.
+				</noscript>
+			</div>-->
 
 		<div class="mb-6 flex flex-row items-center">
 			<div>
@@ -148,7 +293,7 @@
 			</div>
 		</div>
 
-		<div class="md:flex md:items-center">
+		<div class="md:flex md:items-center w-full">
 			<div class="md:w-1/3">
 				{#if browser}
 					{#if valid && datapolicy}
@@ -165,40 +310,31 @@
 					{/if}
 				{/if}
 			</div>
-			<div class="md:w-2/3" />
 		</div>
 	</form>
 	<!-- -->
 </div>
 
 <style>
-	:global(.headlinelink a) {
-		position: relative;
-		display: inline-block;
+	.tab-content {
+		max-height: 0;
+		-webkit-transition: max-height 0.5s;
+		-o-transition: max-height 0.5s;
+		transition: max-height 0.5s;
+	}
+	/* :checked - resize to full height */
+	.tab input:checked ~ .tab-content {
+		max-height: 1000vh;
 	}
 
-	:global(.headlinelink a::after) {
-		background-color: #2a499d;
-		bottom: -2px;
-		content: '';
-		display: block;
-		height: 3px;
-		position: absolute;
-		width: 0%;
-		transition: all;
-		transition-duration: 0.25s;
+	.tab input[type='checkbox'] + label .icon {
+		color: #ea3336;
+		-webkit-transition: all 0.25s;
+		-o-transition: all 0.25s;
+		transition: all 0.25s;
 	}
-
-	:global(.headlinelink a:hover::after) {
-		background-color: #2a499d;
-		bottom: -2px;
-		content: '';
-		display: block;
-		height: 3px;
-		position: absolute;
-		width: 100%;
-		transition: all;
-		transition-duration: 0.25s;
+	.tab input[type='checkbox']:checked + label .icon {
+		color: #6e7071;
 	}
 
 	:global(.dataprivacy a) {
