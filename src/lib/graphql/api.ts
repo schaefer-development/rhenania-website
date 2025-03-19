b153b63b0b760484c80b6e4f5fe38d22461e1781
Robert Schäfer <git@roschaefer.de>
2025-03-19 23:55:06 +0800

    data on index page

--- src/lib/graphql/api.ts
+++ src/lib/graphql/api.ts
@@ -1,24 +1,12 @@
-import type { RequestHandler } from '@sveltejs/kit';
 import { GRAPHCMS_ENDPOINT } from '$lib/env';
-import { fallbackLocale } from '$lib/i18n';
 
-type Api = (query: string, variables?: Record<string, unknown>) => RequestHandler;
-interface HasParams extends Pick<Parameters<RequestHandler>[0], 'params'> {}
-
-type GraphQlRequest = (
-	query: string,
-	variables?: Record<string, unknown>
-) => (event: HasParams) => Promise<Response>;
-
-export const graphQlRequest: GraphQlRequest = (query, variables) => async (event) => {
-	const { lang: requestedLocale } = event.params;
+export const api = async (query: string, variables: Record<string, unknown>) => {
 	const headers = {
 		'Content-Type': 'application/json',
 		Accept: 'application/json',
-		'gcms-locales': [requestedLocale, fallbackLocale].filter(Boolean).join(',')
 	};
 
-	return fetch(GRAPHCMS_ENDPOINT, {
+	const res = await fetch(GRAPHCMS_ENDPOINT, {
 		method: 'POST',
 		headers,
 		body: JSON.stringify({
@@ -26,10 +14,6 @@ export const graphQlRequest: GraphQlRequest = (query, variables) => async (event
 			variables
 		})
 	});
-};
-
-export const api: Api = (query, variables) => async (event) => {
-	const res = await graphQlRequest(query, variables)(event);
 	const { status } = res;
 	const body = await res.json();
 	if (!status.toString().startsWith('2')) {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,13 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
+import type { PageLoad } from './$types';
+import type { Page } from '$lib/graphql/generated/schema'
+import { api } from '$lib/graphql/api'
+import { PAGE } from '$lib/graphql/queries'
+
 export const prerender = true;
+
+
+export const load: PageLoad = async () => {
+  const { body } = await api(PAGE, { slug: 'start' });
+  const page: Page = body.data.page
+  return { page }
+};
