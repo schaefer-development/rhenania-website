90bf3445887d899c09a83b8268adc4c7809d4c4a
Robert Schäfer <git@roschaefer.de>
2025-04-14 23:51:35 +0800

    build: update algoliasearch

--- src/routes/de/suche/+page.server.ts
+++ src/routes/de/suche/+page.server.ts
@@ -1,10 +1,13 @@
 import type { PageServerLoad } from './$types';
-import { index } from '$lib/algolia';
+import { client, INDEX } from '$lib/algolia';
 
 export const load: PageServerLoad = async ({ url }) => {
 	const query = url.searchParams.get('q');
 	if (!query) return { hits: [] };
-	const results = await index.search(query);
+	const results = await client.searchSingleIndex({
+		indexName: INDEX,
+		searchParams: { query }
+	});
 	const { hits } = results;
 	return { hits };
 };
