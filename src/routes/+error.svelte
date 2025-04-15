d824163bfb234ee8d6f287e6e42210820b3c8e74
Robert Schäfer <git@roschaefer.de>
2025-04-15 21:57:59 +0800

    feat: custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+{#if page.status === 404}
+	<h1>404</h1>
+	{#if page.error}
+		<p>Hm, wir können diese Seite leider nicht finden.</p>
+	{/if}
+{:else}
+	<h1>Upsi, da ist was schief gelaufen</h1>
+	{#if page.error}
+		<p>{page.error.message}</p>
+	{/if}
+{/if}
