bbab9d0d287748007750609c689045b82b17b758
joschaefer <mail@joschaefer.com>
2025-06-27 18:51:43 +0200

    fix: styling error in footer => accordion center

--- src/lib/components/Footer/FooterAccordion.svelte
+++ src/lib/components/Footer/FooterAccordion.svelte
@@ -25,11 +25,11 @@
 	});
 </script>
 
-<div class="w-10/12">
+<div class="mx-auto w-10/12">
 	{#each items as item, index (item)}
 		<div class="">
 			<button
-				class="hover:text-rc_red flex w-full cursor-pointer items-center justify-between text-left tracking-wider transition"
+				class="hover:text-rc_red flex w-full cursor-pointer items-center justify-between pb-2 text-left tracking-wider transition"
 				on:click={() => toggleItem(index)}
 			>
 				<span class="grow">{item.title}</span>
@@ -73,7 +73,7 @@
 				<ul class="pb-4 text-white">
 					{#each item.subpoints as sub (sub)}
 						<li class="ml-2 list-disc">
-							<a href={sub.href} class="hover:text-rc_red block pb-1 pl-1 text-white">
+							<a href={sub.href} class="hover:text-rc_red block pb-1 pl-1 text-left text-white">
 								{sub.name}
 							</a>
 						</li>
