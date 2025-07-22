21346017af1290a8536cd945b3686057e2e71dc5
Robert Schäfer <git@roschaefer.de>
2025-07-22 17:50:15 +0700

    feat: clean up temporary indices
    We observe leftover temporary indices from [#replaceAllObjects](https://www.algolia.com/doc/libraries/javascript/v5/helpers/#replace-all-records) in algolia.
    
    I don't know why this happens. I'm not able to reproduce this. Maybe because of concurrent write operations?
    
    This PR will clean up temporary indices after the `replaceAllObjects` has been successful.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
-import { updateIndex } from './searchindex';
+import { updateIndex, cleanTemporaryIndices } from './searchindex';
 
 import type { ServerInit } from '@sveltejs/kit';
 
@@ -7,8 +7,8 @@ export const init: ServerInit = async () => {
 	if (dev) return;
 
 	try {
-		const savedObjects = await updateIndex();
-		console.log('Successfully updated search index.', savedObjects);
+		await updateIndex();
+		await cleanTemporaryIndices();
 	} catch (error) {
 		const message = error instanceof Error ? error.message : error;
 		console.error(message);
